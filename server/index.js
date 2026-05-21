de65a6f4321ce0e7f23e02e1657eaa426151dbd3
Rashad123456 <rashad110726@gmail.comm>
2026-05-21 15:42:59 +0600

    fix: added new vercel preview domain to cors

--- server/index.js
+++ server/index.js
@@ -12,12 +12,11 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://assigment9-meaw1.vercel.app', 
-    'https://assigment9-theta.vercel.app'
+    'https://assigment9-meaw1.vercel.app',
+    'https://assigment9-lb7tnx0dl-meaw1.vercel.app' 
   ],
   credentials: true
 }));
-
 app.use(express.json());
 app.use(cookieParser());
 
